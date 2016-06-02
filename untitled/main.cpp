b1133cafcdbf7453f101113cb0dc46172adb38c2
phamminhkha102 <phamminhkha102@gmail.com>
2016-06-02 09:12:41 +0700

    thay doi lan 2

---
+++ untitled/main.cpp
@@ -0,0 +1,21 @@
+#include <QCoreApplication>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    VideoCapture cap("Wildlife.wmv");
+    Mat mat;
+    while(1){
+        if(cap.read(mat))
+            imshow("Opencv Open Video",mat);
+        waitKey(15);
+///////thay doi lan 2
+    }
+
+    return a.exec();
+}
